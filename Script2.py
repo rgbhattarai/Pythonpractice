3ff38ca541176d0808c926caa406777621af2038
rgbhattarai <ghimirerushie@gmail.com>
2026-08-31 21:54:14 -0400

    Third

---
+++ Script2.py
@@ -0,0 +1,12 @@
+
+## String function practice
+
+a= 'hello World'
+print(a.upper())  ## upper() method change it to uppercase
+print(a.lower()) ## lower() change it to lower case
+
+b= ' hello world!  '
+print(b.strip()) ## The strip() method removes any whitespace from the beginning or the end:
+
+# The replace() method replaces a string with another string:
+print(b.replace('h','j'))
